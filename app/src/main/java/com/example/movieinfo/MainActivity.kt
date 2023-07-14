02cb6f8e36dea1e342a63db0f40eb196524b05c5
Pranay.Jain <pranay.jain@e-zest.in>
2023-07-14 16:45:22 +0530

    API key and token saved

--- app/src/main/java/com/example/movieinfo/MainActivity.kt
+++ app/src/main/java/com/example/movieinfo/MainActivity.kt
@@ -9,3 +9,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+/*
+
+Api
+private const val KEY_ = "8d7b1be7c45d9dc384088a2eddc6224d"
+
+API read access token= eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZDdiMWJlN2M0NWQ5ZGMzODQwODhhMmVkZGM2MjI0ZCIsInN1
+YiI6IjY0YjEyOTk3ZDIzNmU2MDBjNTg2ZmIyMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RVdsX0f3mRegeHPpKvo8tBdh6HT1b0Pa5YbOsFHlRP8*/
